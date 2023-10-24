f7190b714115a578ff90e22850d8ee6c58a5e616
Katie Zeng <katiezeng2000@gmail.com>
2023-10-24 16:40:10 +0100

    created establishment class

---
+++ OOP_Exercise/src/main/java/com/qa/Establishment.java
@@ -0,0 +1,64 @@
+package com.qa;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Establishment {
+    public Establishment() {
+        this("Manchester", 900, 2330, 4.50);
+    }
+    public Establishment(String location, int openTime, int closeTime, double rating) {
+        this.location = location;
+        this.openTime = openTime;
+        this.closeTime = closeTime;
+        this.rating = rating;
+    }
+
+    private String location;
+    private int openTime;
+    private int closeTime;
+    private double rating;
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        if (rating >= 0 && rating <= 5) {
+            this.rating = rating;
+        }
+    }
+
+    public int getCloseTime() {
+        return closeTime;
+    }
+
+    public void setCloseTime(int closeTime) {
+        int closeHour = closeTime / 100;
+        int closeMinute = closeTime % 100;
+        if (closeHour <= 24 && closeMinute < 60 && closeHour >= 0 && closeMinute >= 0) {
+            this.closeTime = closeTime;
+        }
+    }
+
+    public int getOpenTime() {
+        return openTime;
+    }
+
+    public void setOpenTime(int openTime) {
+        int openHour = openTime / 100;
+        int openMinute = openTime % 100;
+        if (openHour <= 24 && openMinute < 60 && openHour >= 0 && openMinute >= 0) {
+            this.openTime = openTime;
+        }
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+}
--- OOP_Exercise/src/main/java/com/qa/Pub.java
+++ OOP_Exercise/src/main/java/com/qa/Pub.java
@@ -4,69 +4,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Pub {
+public class Pub extends Establishment {
 
     public Pub(String location) {
         this(location, 900, 2330, 4.50);
     }
 
     public Pub(String location, int openTime, int closeTime, double rating) {
-        this.location = location;
-        this.openTime = openTime;
-        this.closeTime = closeTime;
-        this.rating = rating;
+        super(location, openTime, closeTime, rating);
         List<String> tempList = Arrays.asList("carling", "madri", "guinness", "g&t", "asahi", "1664", "carlsberg");
         this.drinkList.addAll(tempList);
     }
 
-    private String location;
-    private int openTime;
-    private int closeTime;
-    private double rating;
     private ArrayList<String> drinkList = new ArrayList<>();
 //    private String[] drinks = {"carling", "madri", "guinness", "g&t", "asahi", "1664", "carlsberg"};
 
-    public double getRating() {
-        return rating;
-    }
 
-    public void setRating(double rating) {
-        if (rating >= 0 && rating <= 5) {
-            this.rating = rating;
-        }
-    }
-
-    public int getCloseTime() {
-        return closeTime;
-    }
-
-    public void setCloseTime(int closeTime) {
-        int closeHour = closeTime / 100;
-        int closeMinute = closeTime % 100;
-        if (closeHour <= 24 && closeMinute < 60 && closeHour >= 0 && closeMinute >= 0) {
-            this.closeTime = closeTime;
-        }
-    }
-
-    public int getOpenTime() {
-        return openTime;
-    }
-
-    public void setOpenTime(int openTime) {
-        int openHour = openTime / 100;
-        int openMinute = openTime % 100;
-        if (openHour <= 24 && openMinute < 60 && openHour >= 0 && openMinute >= 0) {
-            this.openTime = openTime;
-        }
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
 
     public void addDrink(String drink) {
         drink = drink.toLowerCase();
